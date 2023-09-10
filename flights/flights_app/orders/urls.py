a180889aa6281126dc15a69cec0814f33dca7751
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2023-09-10 20:51:54 +0300

    calc

--- flights/flights_app/orders/urls.py
+++ flights/flights_app/orders/urls.py
@@ -1,8 +1,14 @@
 from rest_framework.routers import DefaultRouter
 
-from flights_app.orders.views import OrdersViewSet
+from flights_app.orders.views import OrdersViewSet, calc_sum
+
+from django.urls import path
 
 router = DefaultRouter()
 router.register('', OrdersViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('calc', calc_sum)
+]
+
+urlpatterns.extend(router.urls)
--- flights/flights_app/orders/views.py
+++ flights/flights_app/orders/views.py
@@ -1,4 +1,6 @@
+from django.http import JsonResponse
 from rest_framework import mixins
+from rest_framework.decorators import api_view
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 
@@ -16,3 +18,14 @@ class OrdersViewSet(mixins.CreateModelMixin,
     serializer_class = serializers.OrderSerializer
     queryset = models.Order.objects.all()
     permission_classes = [IsAuthenticated, OrdersPermission]
+
+
+@api_view(['GET'])
+def calc_sum(request):
+    num1 = request.GET.get('num1')
+    num2 = request.GET.get('num2')
+
+    # validate
+
+    res = int(num1) + int(num2)
+    return JsonResponse({'result': res})
